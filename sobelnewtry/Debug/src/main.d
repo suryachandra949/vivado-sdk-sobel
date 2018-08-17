5c543c0fbe064fb31d7d76fcbf05410c4d838bc9
suryachandra949 <psuryachandra949@gmail.com>
2018-08-17 12:31:20 +0530

    vivado sdk files for sobel project

---
+++ sobelnewtry/Debug/src/main.d
@@ -0,0 +1,58 @@
+src/main.o src/main.d: ../src/main.cc \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/XAxiDma.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_bd.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_hw.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xdebug.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../src/zebracode.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xnewsob.h \
+ ../../sobelnewtry_bsp/ps7_cortexa9_0/include/xnewsob_hw.h
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/XAxiDma.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_bdring.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_bd.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xaxidma_hw.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xdebug.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../src/zebracode.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xnewsob.h:
+
+../../sobelnewtry_bsp/ps7_cortexa9_0/include/xnewsob_hw.h:
